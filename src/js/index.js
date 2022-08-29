ccbf5d6b103acadd26b2b64fac80754ab7b2a7a7
junhyuk20 <kwk8002@naver.com>
2022-08-29 22:22:38 +0900

    step3 요구사항 정리및 fetch비동기함수 사용

--- src/js/index.js
+++ src/js/index.js
@@ -1,30 +1,24 @@
-// step2 요구사항 구현을 위한 전략
-
-// TODO localStorage Read & Write
-// -[x] localStorage에 데이터를 저장한다.
-// -[x] 메뉴를 추가할 때
-// -[x] 메뉴를 수정할 때
-// -[x] 메뉴를 삭제할 때
-// -[X] localStorag에 있는 데이터를 읽어온다.
-
-// TODO 카테고리별 메뉴판 관리
-// -[x] 에스프레소 메뉴판 관리
-// -[x] 프라푸치노 메뉴판 관리
-// -[x] 블레디드 메뉴판 관리
-// -[x] 티바나 메뉴판 관리
-// -[x] 디저트 메뉴판 관리
-
-// TODO 페이지 접근시 최초 데이터 Read & Rendering
-// -[x] 페이지에 최초로 로딩될때 localStorage에 에스프레소 메뉴를 읽어온다.
-// -[x] 에스프레소 메뉴를 페이지에 그려준다.
-
-// TODO 품질 상태 관리
-// -[x] 품절 상태인 경우를 보여줄 수 있게, 품절 버튼을 추가하고 sold-out class를 추가하여 상태를 변경한다.
-// -[x] 품절 버튼을 버튼을 추가한다.
-// -[x] 품절 버튼을 클릭하면 localStorage에 상태값이 저장된다.
-// -[x] 클릭이벤트에서 가장가까운 li태그의 class속성 값에 sold-out을 추가한다.
 import { $ } from "./utils/dom.js";
 import store from "./store/index.js";
+/*
+step 3 요구사항
+ - [] 웹 서버를 띄운다.
+ - [] 서버에 새로운 메뉴가 추가될 수 있도록 요청한다.
+ - [] 서버에 카테고리별 메뉴리스트를 불러온다.
+ - [] 서버에 메뉴가 수정 될 수 있도록 요청한다.
+ - [] 서버에 메뉴의 품절상태가 토글링 될 수 있도록 요청한다.
+ - [] 서버에 메뉴가 삭제 될 수 있도록 요청한다.
+ 
+ TODO 리팩터링 부분
+ - [] localStorage에 저장하는 로직은 지운다.
+ - [] fetch 비동기 api를 사용하는 부분을 async await를 사용하여 구현한다.
+ 
+ TODO 사용자 경험
+ - [] API 통신이 실패하는 경우에 대해 사용자가 알 수 있게 alert으로 예외처리를 진행한다.
+ - [] 중복되는 메뉴는 추가할 수 없다.
+*/
+
+const BASE_URL = "http://localhost:3000/api";
 
 function App() {
   // 상태는 변하는 데이터, 이 앱에서 변하는 것이 무엇인가? - 메뉴명, 개수(이녀석은 너무 간단해서 제외 해도 된다.) 즉 상태를 관리해야 되는 녀석들은 저장되거나,변하거나 이다.
@@ -96,13 +90,26 @@ function App() {
       return;
     }
     const menuName = $("#menu-name").value;
-    this.menu[this.currentCategory].push({ name: menuName });
-    store.setLocalStorage(this.menu[this.currentCategory]);
+    fetch(`${BASE_URL}/category/${this.currentCategory}/menu`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: menuName }),
+    })
+      .then((response) => {
+        return response.json(); //응답 데이터를 받으려면 json() 메서드를 사용해서 받아야 된다.
+      })
+      .then((res) => {
+        console.log(res);
+      });
 
-    render();
+    //this.menu[this.currentCategory].push({ name: menuName });
+    //store.setLocalStorage(this.menu[this.currentCategory]);
+    //render();
 
     // 메뉴를 추가 후 입력란 초기화
-    $("#menu-name").value = "";
+    //$("#menu-name").value = "";
   };
   const updateMenuName = (e) => {
     const menuId = e.target.closest("li").dataset.menuId; // jQuery .data() 메서드와 동일한 메서드 dataset
